343c2a1148a506ebd2f02cdf4dd2168725c28fbe
qcjxs <1447515520@qq.com>
2023-12-19 16:01:59 +0800

    优化注入程序的获取方式，并且进行babel转换

--- TaroWebContainer/src/main/ets/inject_adapter/ApiAdapter.ts
+++ TaroWebContainer/src/main/ets/inject_adapter/ApiAdapter.ts
@@ -13,6 +13,9 @@
  * limitations under the License.
  */
 import WebView from '@ohos.web.webview'
+import common from '@ohos.app.ability.common';
+import util from '@ohos.util';
+import { GlobalThis } from '../utils/GlobalThis';
 import  { as } from '../utils/advancedapi.min';
 import { wbLogger } from '../utils/Logger';
 
@@ -225,102 +228,19 @@ export class ApiAdapter {
     this.systemBarHeight = height
   }
 
+  getCustomPara(): string {
+    let cuntomParaStr =`var navigationHeight = ${this.navigationBarHeight};
+                 var systemBarHeight = ${this.systemBarHeight};
+                 var customLaunchOptions = '${this.launchOptions}';`
+    return cuntomParaStr
+  }
+
   getRunJavaScript(): string {
-    let result: string = `
-        window.addEventListener('unhandledrejection', (event) => {
-           console.info('[ADSAPI] unhandledrejection: ', event);
-        })
-        if(typeof(adapterInited) === 'undefined'){
-            var asCallbackMap = new Map();
-            var asAPIMap = new Map();
-            var navigationHeight = ${this.navigationBarHeight};
-            var systemBarHeight = ${this.systemBarHeight};
-            var customLaunchOptions = '${this.launchOptions}';
-            var asCallbackId = 0;
-            var receiveTask = (callbackId,resObject) =>{
-                if(asCallbackMap.get(callbackId)){
-                   asCallbackMap.get(callbackId).apply(null,resObject);
-                }
-                return true
-            }
-            var temp_uni = {}
-            native = new Proxy({}, {
-                get(target,property){
-                    if(asAPIMap.has(property)){
-                        return asAPIMap.get(property)
-                    }
-                    return temp_uni[property]
-                }
-            })
-
-            var getCallbackParam = (args) =>{
-                const callbackObj = []
-                args.forEach((arg,index)=>{
-                    if(typeof(arg) === 'object') {
-                        callbackObj[index] = {}
-                        for(let name in arg){
-                            if(typeof(arg[name])==='function'){
-                               asCallbackMap.set(asCallbackId,arg[name]);
-                               callbackObj[index][name] = asCallbackId++;
-                            }
-                        }
-                    } else if (typeof(arg) === 'function') {
-                        let tempId = null;
-                        for(let prop of asCallbackMap){
-                            if(prop[1] === arg){
-                                tempId = prop[0];
-                                break;
-                            }
-                        }
-                        if (tempId !== null) {
-                            callbackObj[index] = tempId + '_' + (arg.name ? arg.name : 'func');
-                        } else {
-                            asCallbackMap.set(asCallbackId,arg);
-                            callbackObj[index] = asCallbackId + '_' + (arg.name ? arg.name : 'func');
-                            asCallbackId++;
-                        }
-
-                    }
-                })
-                const argsStr = JSON.stringify(args);
-                const callbackString = JSON.stringify(callbackObj);
-                return [callbackString, argsStr]
-            }
-
-            var setAsAPIFunction = (method) =>{
-                return (...args) => {
-                    const callbackParam = getCallbackParam(args)
-                    const result = as[method](callbackParam[0], callbackParam[1])
-                    if(result){
-                        const resultObj = JSON.parse(result)
-                        const resultObjT = JSON.parse(result)
-                        const asObjectId = resultObj.asObjectId
-                        const proxy = new Proxy(resultObjT,{
-                            get:(target,property)=>{
-                                const type = resultObj[property]
-                                if(type ==='function'){
-                                    return (...args1) => {
-                                        const callbackParam = getCallbackParam(args1)
-                                        return as.sendToAs(asObjectId,'get_function',property,callbackParam[0], callbackParam[1])
-                                    }
-                                } else {
-                                    return as.sendToAs(asObjectId,'get_value',property)
-                                }
-                            },
-                            set:(target,property,value)=>{
-                                as.sendToAs(asObjectId,'set',property,value)
-                                return true
-                            }
-                        })
-                        return proxy
-                    } else if (typeof(result) === 'boolean'){
-                        return result
-                    }
-                }
-            }
-            var adapterInited = true;
-        }
-        `
+    let context = GlobalThis.getInstance().getContext('context') as common.UIAbilityContext;
+    let fileContent = context.resourceManager.getRawFileContentSync('injectTransform.js')
+    let textDecoder = util.TextDecoder.create("utf-8", { ignoreBOM: true });
+    let result = textDecoder.decodeWithStream(fileContent, { stream: false });
+    result = result.concat(this.getCustomPara())
     this.getMethodList().forEach(method => {
       result = result + this.createJavaScriptString(method)
     })
