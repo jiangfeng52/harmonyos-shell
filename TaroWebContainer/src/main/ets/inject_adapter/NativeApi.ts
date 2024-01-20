99d475ef2dc1ad2aea2aafb3ad9ae626521affd7
chenai02 <1766431377@qq.com>
2024-01-20 16:29:14 +0800

    修改app.js文件内容，为修改后的通道做测试

--- TaroWebContainer/src/main/ets/inject_adapter/NativeApi.ts
+++ TaroWebContainer/src/main/ets/inject_adapter/NativeApi.ts
@@ -144,53 +144,96 @@ interface NativeApi {
 
   getImageInfoBridgeAsync(options: ESObject): void
 
-  compressVideoBridgeAsync(options: ESObject): void
-
-  createInnerAudioContextBridgeAsync(options: ESObject): void
-
   getLocationBridgeAsync(options: ESObject): void
 
   openDocumentBridgeAsync(options: ESObject): void
 
-  loginBridgeAsync(options: ESObject): void
-
-  getUserInfoBridgeAsync(options: ESObject): void
+  getUserInfoBridgeSync(options: ESObject): void
 
   onUserCaptureScreenBridgeSync(options: ESObject): void
 
   hideKeyboardBridgeAsync(options: ESObject): void
 
-  getFileSystemManagerBridgeSync(options: ESObject): ESObject
+  accessBridgeAsync(options: ESObject): ESObject
+
+  saveFileBridgeAsync(options: ESObject): ESObject
+
+  getFileInfoBridgeAsync(options: ESObject): ESObject
+
+  readFileBridgeAsync(options: ESObject): ESObject
+
+  readFileSyncBridgeSync(options: ESObject): ESObject
+
+  createInnerAudioContextBridgeSync(options: ESObject): void
+
+  stopBridgeSync(): void
+
+  playBridgeSync(): void
+
+  onPlayBridgeSync(options: ESObject): ESObject
+
+  onStopBridgeSync(options: ESObject): ESObject
+
+  onErrorBridgeSync(options: ESObject): ESObject
+
+  onEndedBridgeSync(options: ESObject): ESObject
+
+  abortBridgeSync(options: ESObject): ESObject
+
+  offHeadersReceivedBridgeSync(options: ESObject): ESObject
+
+  offProgressUpdateBridgeSync(options: ESObject): ESObject
+
+  onHeadersReceivedBridgeSync(options: ESObject): ESObject
+
+  onProgressUpdateBridgeSync(options: ESObject): ESObject
 }
 
 class NativeApiImpl implements NativeApi {
+  private static fileManager: any
+  private innerAudioContext: any
+  private uploadTaskMap: Map<number, any>
+  private uploadTaskID: number
+
+  constructor() {
+    this.uploadTaskMap = new Map()
+    this.uploadTaskID = 0
+
+  }
+  private static getFileManager() {
+    if (!NativeApiImpl.fileManager) {
+      NativeApiImpl.fileManager = as.getFileSystemManager()
+    }
+    return NativeApiImpl.fileManager
+  }
+
   getWindowInfoBridgeSync(): WindowInfo {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getWindowInfo`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getWindowInfo`)
     return as.getWindowInfo()
   }
 
   getSystemInfoSyncBridgeSync(options: ESObject): SystemInfoSync {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getSystemInfoSync`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getSystemInfoSync`)
     return as.getSystemInfoSync()
   }
 
   getSystemSettingBridgeSync(options: ESObject): SettingInfo {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getSystemSetting`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getSystemSetting`)
     return as.getSystemSetting()
   }
 
   getAppBaseInfoBridgeSync(options: ESObject): AppBaseInfo {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getAppBaseInfo`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getAppBaseInfo`)
     return as.getAppBaseInfo()
   }
 
   getAppAuthorizeSettingBridgeSync(options: ESObject): AppAuthorizeSetting {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getAppAuthorizeSetting`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getAppAuthorizeSetting`)
     return as.getAppAuthorizeSetting()
   }
 
   requestBridgeAsync(options: ESObject): ESObject {
-    wbLogger.debug('JsBridgeNative', `NativeApi$request`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$request`)
     return as.request({
       ...options,
       success: options.success,
@@ -199,7 +242,7 @@ class NativeApiImpl implements NativeApi {
   }
 
   downloadFileBridgeAsync(options: ESObject): ESObject {
-    wbLogger.debug('JsBridgeNative', `NativeApi$downloadFile`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$downloadFile`)
     return as.downloadFile({
       ...options,
       success: options.success,
@@ -207,18 +250,8 @@ class NativeApiImpl implements NativeApi {
     })
   }
 
-  uploadFileBridgeAsync(options: ESObject): ESObject {
-    wbLogger.debug('JsBridgeNative', `NativeApi$uploadFile`)
-    return as.uploadFile({
-      ...options,
-      success: options.success,
-      fail: options.fail,
-      complete: options.complete
-    })
-  }
-
   saveImageToPhotosAlbumBridgeAsync(options: ESObject): ESObject {
-    wbLogger.debug('JsBridgeNative', `NativeApi$saveImageToPhotosAlbum`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$saveImageToPhotosAlbum`)
     return as.saveImageToPhotosAlbum({
       ...options,
       success: options.success,
@@ -227,8 +260,8 @@ class NativeApiImpl implements NativeApi {
   }
 
   getVideoInfoBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getVideoInfo`)
-    return as.getImageInfo({
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getVideoInfo`)
+    return as.getVideoInfo({
       ...options,
       success: options.success,
       fail: options.fail
@@ -236,7 +269,7 @@ class NativeApiImpl implements NativeApi {
   }
 
   getImageInfoBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getImageInfo`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getImageInfo`)
     return as.getImageInfo({
       ...options,
       success: options.success,
@@ -244,22 +277,8 @@ class NativeApiImpl implements NativeApi {
     })
   }
 
-  compressVideoBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$compressVideo`)
-    return as.compressVideo({
-      ...options,
-      success: options.success,
-      fail: options.fail
-    })
-  }
-
-  createInnerAudioContextBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$createInnerAudioContext`)
-    return as.createInnerAudioContext(options)
-  }
-
   getLocationBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getLocation`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getLocation`)
     return as.getLocation({
       ...options,
       success: options.success,
@@ -268,7 +287,7 @@ class NativeApiImpl implements NativeApi {
   }
 
   openDocumentBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$openDocument`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$openDocument`)
     return as.openDocument({
       ...options,
       success: options.success,
@@ -276,23 +295,13 @@ class NativeApiImpl implements NativeApi {
     })
   }
 
-  loginBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$login`)
-    return as.login(options)
-  }
-
-  getUserInfoBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getUserInfo`)
+  getUserInfoBridgeSync(options: ESObject): void {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getUserInfo`)
     return as.getUserInfo(options)
   }
 
-  getFileSystemManagerBridgeSync(options: ESObject): ESObject {
-    wbLogger.debug('JsBridgeNative', `NativeApi$getFileSystemManager`)
-    return as.getFileSystemManager(options)
-  }
-
   setKeepScreenOnBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$setKeepScreenOn`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$setKeepScreenOn`)
     return as.setKeepScreenOn({
       ...options,
       success: options.success,
@@ -302,22 +311,158 @@ class NativeApiImpl implements NativeApi {
 
   //回调监听
   onUserCaptureScreenBridgeSync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$onUserCaptureScreen`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onUserCaptureScreen`)
     return as.onUserCaptureScreen(options)
   }
 
   hideKeyboardBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$hideKeyboard`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$hideKeyboard`)
     return as.hideKeyboard(options)
   }
 
   makePhoneCallBridgeAsync(options: ESObject): void {
-    wbLogger.debug('JsBridgeNative', `NativeApi$makePhoneCall`)
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$makePhoneCall`)
     return as.makePhoneCall({
       ...options,
       success: options.success,
       fail: options.fail
     })
+  }
+
+  saveFileBridgeAsync(options: ESObject): ESObject {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$access`)
+    return as.saveFile({
+      ...options,
+      success: options.success,
+      fail: options.fail
+    })
+  }
+
+  accessBridgeAsync(options: ESObject): ESObject {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$access`)
+    return NativeApiImpl.getFileManager().access({
+      ...options,
+      success: options.success,
+      fail: options.fail,
+      complete: options.complete
+    })
+  }
+
+  getFileInfoBridgeAsync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$getFileInfo`)
+    return NativeApiImpl.getFileManager().getFileInfo({
+      ...options,
+      success: options.success,
+      fail: options.fail,
+      complete: options.complete
+    })
+  }
+
+  readFileBridgeAsync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$readFile`)
+    return NativeApiImpl.getFileManager().readFile({
+      ...options,
+      success: options.success,
+      fail: options.fail,
+      complete: options.complete
+    })
+  }
+
+  readFileSyncBridgeSync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$readFileSync`)
+    return NativeApiImpl.getFileManager().readFileSync(options)
+  }
+
+  createInnerAudioContextBridgeSync(options: any): void {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$createInnerAudioContext`)
+    this.innerAudioContext = as.createInnerAudioContext()
+  }
+
+  stopBridgeSync(): void {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$stop`)
+    this.innerAudioContext.stop()
+  }
+
+  playBridgeSync(): void {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$play`)
+    this.innerAudioContext.play()
+  }
+
+  onPlayBridgeSync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onPlay`)
+    this.innerAudioContext.onPlay(options)
+  }
+
+  onStopBridgeSync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onStop`)
+    this.innerAudioContext.onStop(options)
+  }
+
+  onErrorBridgeSync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onError`)
+    this.innerAudioContext.onError(options)
+  }
+
+  onEndedBridgeSync(options: any): any {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onEnded`)
+    this.innerAudioContext.onEnded(options)
+  }
+
+  uploadFileBridgeAsync(options: ESObject): ESObject {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$uploadFile`)
+    const uploadTask = as.uploadFile({
+      ...options,
+      success: options.success,
+      fail: options.fail,
+      complete: options.complete
+    })
+    this.uploadTaskMap.set(this.uploadTaskID, uploadTask)
+    return this.uploadTaskID ++
+  }
+
+  abortBridgeSync(options: ESObject): ESObject {
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$abort`)
+    if (!this.uploadTaskMap.has(options.taskID)){
+      wbLogger.error('JsBridgeNativeApi', 'the uploadTask has not been init')
+      return
+    }
+    this.uploadTaskMap.get(options.taskID).abort()
+  }
+
+  offHeadersReceivedBridgeSync(options: ESObject): ESObject{
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$offHeadersReceived`)
+    if (!this.uploadTaskMap.has(options.taskID)){
+      wbLogger.error('JsBridgeNativeApi', 'the uploadTask has not been init')
+      return
+    }
+    this.uploadTaskMap.get(options.taskID).offHeadersReceived(options)
+  }
+
+  offProgressUpdateBridgeSync(options: ESObject): ESObject{
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$offProgressUpdate`)
+    if (!this.uploadTaskMap.has(options.taskID)){
+      wbLogger.error('JsBridgeNativeApi', 'the uploadTask has not been init')
+      return
+    }
+    this.uploadTaskMap.get(options.taskID).offProgressUpdate(options)
+  }
+
+  onHeadersReceivedBridgeSync(options: ESObject): ESObject{
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onHeadersReceived`)
+    if (!this.uploadTaskMap.has(options.taskID)){
+      wbLogger.error('JsBridgeNativeApi', 'the uploadTask has not been init')
+      return
+    }
+    this.uploadTaskMap.get(options.taskID).onHeadersReceived(options)
+  }
+
+  onProgressUpdateBridgeSync(options: ESObject): ESObject{
+    wbLogger.debug('JsBridgeNativeApi', `NativeApi$onProgressUpdate`)
+    if (!this.uploadTaskMap.has(options.taskID)){
+      wbLogger.error('JsBridgeNativeApi', 'the uploadTask has not been init')
+      return
+    }
+    this.uploadTaskMap.get(options.taskID).onProgressUpdate(options)
   }
 }
 
--- TaroWebContainer/src/main/resources/rawfile/app.js
+++ TaroWebContainer/src/main/resources/rawfile/app.js
@@ -1,4 +1,3 @@
-
 // TODO-ly 此类都需要重写，因为此类是es5的js
 
 function isFunctionOrObjectWithFunction(object) {
@@ -13,6 +12,7 @@ function isFunctionOrObjectWithFunction(object) {
 
     // 如果是对象，遍历对象的每个属性，检查属性是否是函数
     for (const key in object) {
+        console.debug('nativeapi', 'isFunctionOrObjectWithFunction the key: ' + key)
         if (object.hasOwnProperty(key) && typeof object[key] === 'function') {
             return true;
         }
@@ -20,7 +20,7 @@ function isFunctionOrObjectWithFunction(object) {
     return false
 }
 
-function isOnMehtod(methodName){
+function isOnMethod(methodName) {
     const regex = /^on[A-Z]\w*$/;
     if (regex.test(methodName)) {
         return true // 以on开头的方法调用
@@ -39,7 +39,7 @@ window.Channel = {
     /**
      * methodCall的对象结构为：{call: string, argsJson: string, stubId: number}
      */
-    nativeCall: function(channelType, object){
+    nativeCall: function (channelType, object) {
         // 最终调用原生Channel的通信方法
         var objectJson = JSON.stringify(object)
         // @ts-ignore
@@ -50,7 +50,7 @@ window.Channel = {
     /**
      * 给原生暴露的通信方法，原生调用"window.Channel.jsCall(objectId, 'xxx', 'xxxx')"
      */
-    jsCall: function(channelType, objectJson){
+    jsCall: function (channelType, objectJson) {
         console.log(`jsCall channelType=${channelType}, objectJson=${objectJson}`);
         // window.apiStubPool.onTransact(objectId, callName, argsJson)
         var fun = this.jsCallListeners[channelType]
@@ -61,46 +61,56 @@ window.Channel = {
 window.MethodChannel = {
     ChannelType: 'MethodChannel',
 
-    init: function (){
-        window.Channel.registerJsCall(this.ChannelType, function (object){
+    init: function () {
+        window.Channel.registerJsCall(this.ChannelType, function (object) {
             window.MethodChannel.__ArgsMethodStub(object)
         })
     },
+    //提交了一次到本地，只修改了app.js文件
+    createNativeApiProxy: function (nativeApi) {
+        return new Proxy(nativeApi, {
+            get(target, prop, receiver) {
+                if (typeof target[prop] === 'function') {
+                    const Func = target[prop]
+                    // const isSync = prop.toString().endsWith("BridgeSync")
+                    // const isAsync = prop.toString().endsWith("BridgeAsync")
+                    // if(isSync || isAsync) { // 走代理
+                    const className = target.constructor.name;
+                    return function (...args) {
+                        const isAsync = args[0]?.isAsync
+                        const autoRelease = args[0]?.autoRelease
+                        if (isAsync == 'undefined') {
+                            return Func()
+                        }
+                        const firstArg = args.length > 1 ? args[1] : ''
+                        // 约定异步回调的方式，一次性回调，监听回调
+                        let isListener = isOnMethod(prop.toString())
+                        let hasFun = isFunctionOrObjectWithFunction(firstArg)
 
-    createNativeApiProxy: function (nativeApi){
-      return new Proxy(nativeApi, {
-          get(target, prop, receiver) {
-              if(typeof target[prop] === 'function') {
-                  const isSync = prop.toString().endsWith("BridgeSync")
-                  const isAsync = prop.toString().endsWith("BridgeAsync")
-                  if(isSync || isAsync) { // 走代理
-                      const className = target.constructor.name;
-                      return function (...args) {
-                          const firstArg = args.length >= 1 ? args[0] : ''
-                          // 约定异步回调的方式，一次性回调，监听回调
-                          let isListener = isOnMehtod(prop.toString())
-                          let hasFun = isFunctionOrObjectWithFunction(firstArg)
+                        // 方法调用转换为数据
+                        var methodCall = {
+                            //修改了名称
+                            isAsync: isAsync,
+                            call: `${className ? className : ''}\$${prop.toString()}`,
+                            isListener: isListener,
+                            arg: firstArg,
+                            //增加一个入参
+                            stubId: hasFun ? window.MethodChannel.__registerArgStub(firstArg, isListener) : -1,
+                            //增加了字段，channel端需要做适配
+                            autoRelease: autoRelease
+                        }
+                        return window.Channel.nativeCall(window.MethodChannel.ChannelType, methodCall)
+                    }
+                }
+                // }
 
-                          // 方法调用转换为数据
-                          var methodCall = {
-                              isSync: isSync,
-                              call: `${className?className:''}\$${prop.toString()}`,
-                              isListener: isListener,
-                              arg: firstArg,
-                              stubId: hasFun ? window.MethodChannel.__registerArgStub(firstArg, isListener) : -1
-                          }
-                          return window.Channel.nativeCall(window.MethodChannel.ChannelType, methodCall)
-                      }
-                  }
-              }
-
-              // 直接返回
-              return target[prop];
-          }
-      });
+                // 直接返回
+                return target[prop];
+            }
+        });
     },
 
-    _NextId: 0,// 初始ID值
+    _NextId: 0, // 初始ID值
     _argsStubMap: {},
     _listenerMap: {}, // TODO 弱引用自动取消
     __registerArgStub: function (arg, isListener) {
@@ -112,23 +122,27 @@ window.MethodChannel = {
         }
         return objectId
     },
-    __ArgsMethodStub: function (object){
+    __ArgsMethodStub: function (object) {
         const {call, args, stubId, isListener} = object
-
-        if(isListener) {
+        if (isListener) {
             const listener = this._listenerMap[stubId]
             listener && listener(...args)
 
             return
         }
-        
+
         const argsStub = this._argsStubMap[stubId];
-        if(!argsStub) {
-            return ;
+        console.debug('nativeapi', 'appjs the __registerArgStub object JSON: ' + JSON.stringify(object))
+        if (!argsStub) {
+            console.debug('nativeapi', 'appjs the __registerArgStub argsStub hash been deleted: ')
+            return;
         }
         // 只回调一次，可能会有问题，需测试
         delete this._argsStubMap[stubId]
-
+        if (args[0] === null) {
+            argsStub[call].call(argsStub)
+            return
+        }
         argsStub[call].call(argsStub, ...args)
     }
 }
