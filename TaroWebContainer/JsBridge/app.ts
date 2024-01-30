78dfe6bf88740ddeb1df8a77ffa24595616dd551
chenai02 <1766431377@qq.com>
2024-01-30 11:00:33 +0800

    feat:将通信通道js侧内容转移到ts文件内，并将装饰器内容也转移其中

---
+++ TaroWebContainer/JsBridge/app.ts
@@ -0,0 +1,171 @@
+// TODO-ly 此类都需要重写，因为此类是es5的js
+
+function isFunction(object: any): boolean {
+  if (typeof object === 'function') {
+    // 如果是函数，直接返回 true
+    return true;
+  }
+  return false;
+}
+
+function getAllFuns(object: any): string[] {
+  if (typeof object !== 'object' || object === null) {
+    return [];
+  }
+
+  const funs = []
+  // 如果是对象，遍历对象的每个属性，检查属性是否是函数
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && typeof object[key] === 'function') {
+      funs.push(key)
+    }
+  }
+  return funs
+}
+
+function isFunctionOrObjectWithFunction(object: any): boolean {
+  if (isFunction(object)) {
+    return true
+  }
+
+  if (typeof object !== 'object' || object === null) {
+    return false;
+  }
+
+  // 如果是对象，遍历对象的每个属性，检查属性是否是函数
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && typeof object[key] === 'function') {
+      return true;
+    }
+  }
+  return false
+}
+
+
+// 通信Channel层
+// @ts-ignore
+window.Channel = {
+  jsCallListeners: new Map(),
+  // listenerMap: {}, 弱引用自动取消
+  registerJsCall(channelType: string, fun: Function) {
+    this.jsCallListeners.set(channelType, fun)
+  },
+
+  /**
+   * methodCall的对象结构为：{call: string, argsJson: string, stubId: number}
+   */
+  nativeCall: function (channelType: string, object: any) {
+    // 最终调用原生Channel的通信方法
+    var objectJson = JSON.stringify(object)
+    // @ts-ignore
+    var resultJson = window.JSBridge && window.JSBridge.nativeMethod(channelType, objectJson)
+    return resultJson && JSON.parse(resultJson)
+  },
+
+  /**
+   * 给原生暴露的通信方法，原生调用"window.Channel.jsCall(objectId, 'xxx', 'xxxx')"
+   */
+  jsCall: function (channelType: string, object: any) {
+    console.log(`jsCall channelType=${channelType}, objectJson=${JSON.stringify(object)}`);
+    var fun = this.jsCallListeners.get(channelType)
+    fun && fun(object)
+  }
+}
+// @ts-ignore
+window.MethodChannel = {
+  ChannelType: 'MethodChannel',
+
+  init: function () {
+    // @ts-ignore
+    window.Channel.registerJsCall(this.ChannelType, function (object) {
+      // @ts-ignore
+      window.MethodChannel.__ArgsMethodStub(object)
+    })
+  },
+  jsBridgeMode: function (mode: { isAsync: boolean, autoRelease?: boolean }) {
+    return function (target: any, key: string, descriptor: PropertyDescriptor) {
+      const className = target.constructor.name
+      descriptor.value = function (...args: any[]) {
+
+        const firstArg = args.length >= 1 ? args[0] : ''
+        const objectId = args.length >= 2 ? args[1] : undefined
+
+        let argTypeIsFun = isFunction(firstArg)
+        // @ts-ignore
+        let stubId = window.MethodChannel.__registerArgStub(firstArg, argTypeIsFun, mode?.autoRelease ?? true)
+        if (argTypeIsFun) {
+          // @ts-ignore
+          if (window.MethodChannel._listenerMap.has(firstArg)) {
+            // @ts-ignore
+            stubId = window.MethodChannel._listenerMap.get(firstArg)
+          } else {
+            // @ts-ignore
+            window.MethodChannel._listenerMap.set(firstArg, stubId)
+          }
+        }
+
+        // 方法调用转换为数据
+        var methodCall = {
+          //修改了名称
+          isAsync: mode?.isAsync ?? true,
+          // 调用函数名
+          call: `${className ? className : ''}\$${key.toString()}`,
+          arg: {
+            isFun: argTypeIsFun,
+            properties: firstArg,
+            funs: getAllFuns(firstArg), // ['success', 'fail']
+            stubId: stubId,
+            objectId: objectId,
+          },
+        }
+        // @ts-ignore
+        return window.Channel.nativeCall(window.MethodChannel.ChannelType, methodCall)
+      }
+    }
+  },
+
+  _NextId: 0, // 初始ID值
+  _stubMap: new Map(),
+  _listenerMap: new Map(),
+  __registerArgStub: function (argObject: any, isFun: boolean, autoRelease: boolean) {
+    const hasFun = isFunctionOrObjectWithFunction(argObject)
+    if (!hasFun) {
+      return -1
+    }
+    var objectId = this._NextId++
+    this._stubMap.set(objectId, {
+      object: argObject,
+      isFun: isFun,
+      autoRelease: autoRelease
+    })
+    return objectId
+  },
+  __ArgsMethodStub: function (nativeArg: any) {
+    const {call, args, stubId} = nativeArg
+    const stub = this._stubMap.get(stubId);
+    if (!stub) {
+      console.debug('nativeapi', 'appjs argsStub hash been deleted ')
+      return;
+    }
+    const {object, isFun, autoRelease} = stub
+    if (autoRelease) {
+      this._stubMap.delete(stubId)
+    }
+    else if (call == 'complete') {
+      this._stubMap.delete(stubId)
+    }
+
+    if (isFun) {
+      object && object(...args)
+      return
+    }
+    if (args) {
+      object[call].call(object, ...args)
+    } else {
+      object[call].call(object)
+    }
+  }
+}
+// @ts-ignore
+window.MethodChannel.init()
+
--- TaroWebContainer/hvigorfile.ts
+++ TaroWebContainer/hvigorfile.ts
@@ -1,6 +1,35 @@
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
+import { transformSync } from '@babel/core';
+import * as fs from 'fs'
 
 export default {
-    system: harTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+  system: harTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: [customPluginFunction()]         /* Custom plugin to extend the functionality of Hvigor. */
+}
+
+export function customPluginFunction(str?: string) {
+  return {
+    pluginId: 'CustomPluginID',
+    apply(pluginContext) {
+      pluginContext.registerTask({
+        // 编写自定义任务
+        name: 'tsToJsModule',
+        run: (taskContext) => {
+          const path = require('path');
+          let appFilePath = path.join(taskContext.modulePath, "JsBridge", "app.ts")
+
+          let rawFileContent = fs.readFileSync(appFilePath, 'utf8')
+          const result = transformSync(rawFileContent, {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-typescript"],
+          })
+
+          let outputFilePath = path.join(taskContext.modulePath, "src", "main", "resources", "rawfile", "app.js")
+          fs.writeFileSync(outputFilePath, result.code)
+        },
+        // 确认自定义任务插入位置
+        postDependencies: ['default@PreBuild']
+      })
+    }
+  }
 }
